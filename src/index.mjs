0d1febb9dc513427742ec5c6cd30d7286f5fe56c
odan <odan3240@gmail.com>
2023-06-14 01:56:37 +0900

    add download logic

--- src/index.mjs
+++ src/index.mjs
@@ -1,1 +1,36 @@
-console.log("hello");
+import path from "node:path";
+import fs from "node:fs/promises";
+import { downloadTool } from "@actions/tool-cache";
+import { addPath } from "@actions/core";
+
+const urls = {
+  linux: {
+    amd64:
+      "https://github.com/line/centraldogma-go/releases/download/0.1.0/dogma.linux-amd64",
+  },
+  darwin: {
+    amd64:
+      "https://github.com/line/centraldogma-go/releases/download/0.1.0/dogma.darwin-amd64",
+    arm64:
+      "https://github.com/line/centraldogma-go/releases/download/0.1.0/dogma.darwin-arm64",
+  },
+  windows: {
+    amd64:
+      "https://github.com/line/centraldogma-go/releases/download/0.1.0/dogma.windows-amd64.exe",
+  },
+};
+
+const os = "darwin";
+const arch = "amd64";
+
+const url = urls[os][arch];
+
+const downloadDogmaPath = await downloadTool(url);
+
+const dogmaDir = path.dirname(downloadDogmaPath);
+const ext = path.extname(downloadDogmaPath);
+const dogmaPath = path.join(dogmaDir, `dogma${ext}`);
+
+await fs.rename(downloadDogmaPath, path.join(dogmaDir, "dogma"));
+
+addPath(dogmaPath);
